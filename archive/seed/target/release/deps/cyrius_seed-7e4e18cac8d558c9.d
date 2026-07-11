af2891f63dea0bc94db5281626757cb6ea066ec9
t <t@t>
2026-07-10 21:32:11 -0700

    cleanup targe sign

--- archive/seed/target/release/deps/cyrius_seed-7e4e18cac8d558c9.d
+++
@@ -1,5 +0,0 @@
-/home/macro/Repos/cyrius/archive/seed/target/release/deps/cyrius_seed-7e4e18cac8d558c9.d: src/main.rs
-
-/home/macro/Repos/cyrius/archive/seed/target/release/deps/cyrius_seed-7e4e18cac8d558c9: src/main.rs
-
-src/main.rs:
--- archive/seed/target/release/deps/cyrius_seed-c027900ae07533e9.d
+++
@@ -1,5 +0,0 @@
-/home/macro/Repos/cyrius/archive/seed/target/release/deps/cyrius_seed-c027900ae07533e9.d: src/main.rs
-
-/home/macro/Repos/cyrius/archive/seed/target/release/deps/cyrius_seed-c027900ae07533e9: src/main.rs
-
-src/main.rs:
